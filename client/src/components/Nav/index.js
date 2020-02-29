916e5fb179758332bccc39fe1e362bf5842065f0
Bessy Gee <nakiwarai13@gmail.com>
2020-02-29 10:45:57 -0500

    added Nav bar component

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <a className="navbar-brand" href="/">
+                <h2 className="text-white">Google Book Search</h2>
+            </a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+            aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item" id="home">
+                        <a className="nav-link" href="/">Search Books</a>
+                    </li>
+                    <li className="nav-item" id="report">
+                        <a className="nav-link" href="/saved">Saved Books</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+ export default Nav;
