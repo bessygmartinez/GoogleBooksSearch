17e0690f9af005019e5b84fef20d9590880d3be6
Bessy Gee <nakiwarai13@gmail.com>
2020-02-29 10:48:14 -0500

    added Footer component

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+	<footer className="footer">
+		<div className="container">
+			<span className="text-muted">Copyright © 2020 Bessy G. Martinez. All Rights Reserved</span>
+		</div>	
+	</footer>	
+);
+
+export default Footer;
