5ce32f14ba2e27eca78b46ac256cb64e3017c644
Bessy Gee <nakiwarai13@gmail.com>
2020-03-02 15:10:26 -0500

    added sort to findAll method to sort by Author

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -4,6 +4,7 @@ const db = require("../models");
 module.exports = {
   findAll: function(req, res) {
     db.Book.find(req.query)
+      .sort({"authors": 1})
       .then(dbBook => res.json(dbBook))
       .catch(err => res.status(422).json(err));
   },
