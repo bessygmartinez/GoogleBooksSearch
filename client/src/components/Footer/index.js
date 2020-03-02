778eaa9f1a262f6342c11374369e4539eba4506d
Bessy Gee <nakiwarai13@gmail.com>
2020-03-02 15:04:43 -0500

    added zIndex styling to footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 const Footer = () => (
-	<footer className="footer">
+	<footer className="footer" style={{"zIndex": "10000"}}>
 		<div className="container">
 			<span className="text-muted">Copyright © 2020 Bessy G. Martinez. All Rights Reserved</span>
 		</div>	
