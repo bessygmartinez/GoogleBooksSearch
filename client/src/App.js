89c1468366bcc0fb17938e3d769f73e0f6cc3101
Bessy Gee <nakiwarai13@gmail.com>
2020-03-02 15:03:32 -0500

    added zIndex styling to toast, corrected typo in redirect to /search

--- client/src/App.js
+++ client/src/App.js
@@ -4,50 +4,49 @@ import { Route, Redirect, Switch } from "react-router-dom";
 import SaveBooks from "./pages/SaveBooks";
 import SearchBooks from "./pages/SearchBooks";
 import Nav from "./components/Nav";
-import Jumbotron from "./components/Jumbotron"
-import Footer from "./components/Footer"
-import "./App.css"
+import Jumbotron from "./components/Jumbotron";
+import Footer from "./components/Footer";
+import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
 
-
 class App extends Component {
-  render () {
+  render() {
     return (
-    <React.Fragment>
-
+      <div>
         <header>
-        <Nav />
+          <Nav />
         </header>
 
         <main role="main" className="container">
-        <ToastContainer 
-      position="top-center"
-      autoClose={1500}
-      hideProgressBar
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnVisibilityChange
-      draggable
-      pauseOnHover
-      id="11"
-      role="alert"
-      transition={Slide}
-      />
-        <Jumbotron />
-        <Switch>
-          <Route path="/search" component={SearchBooks} />
-          <Route path="/saved" component={SaveBooks} />
-          {/* <Route path="/not-found" component={NotFound} /> */}
-          <Redirect from="/" exa to="/search" />
-          {/* <Redirect to="/not-found" /> */}
-        </Switch>
+          <ToastContainer
+            position="top-center"
+            autoClose={1500}
+            hideProgressBar
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnVisibilityChange
+            draggable
+            pauseOnHover
+            id="11"
+            role="alert"
+            transition={Slide}
+            style={{"zIndex": "11000"}}
+          />
+          <Jumbotron />
+          <Switch>
+            <Route path="/search" component={SearchBooks} />
+            <Route path="/saved" component={SaveBooks} />
+            {/* <Route path="/not-found" component={NotFound} /> */}
+            <Redirect from="/" exact to="/search" />
+            {/* <Redirect to="/not-found" /> */}
+          </Switch>
         </main>
-        
-          <Footer />
-    </React.Fragment>
-  );
-}
+
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
